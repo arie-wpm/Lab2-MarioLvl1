1858c2de87cb7f4baeeb9e46877539cb9c2a6861
Arie Maljers <ariemaljers@gmail.com>
2026-02-24 16:13:27 +1100

    Added Score Manager + State Events
    Commented some too

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class StateManager : MonoBehaviour
 {
@@ -7,15 +8,34 @@ public class StateManager : MonoBehaviour
         StartScreen,
         Play,
         PauseScreen,
-
-        Invinsible,
+        Invincible,
 
         FireFlower,
         Dead,
         Won,
     }
 
+    // State Related Events Invoked whenever a state is entered.
     public static GameState CurrentState;
+
+    public static UnityEvent EnteredStartScreen;
+    public static UnityEvent EnteredPlay;
+    public static UnityEvent EnteredPauseScreen;
+    public static UnityEvent EnteredInvincible;
+    public static UnityEvent EnteredFireFlower;
+    public static UnityEvent EnteredDead;
+    public static UnityEvent EnteredWon;
+
+    void OnEnable()
+    {
+        EnteredStartScreen ??= new UnityEvent();
+        EnteredPlay ??= new UnityEvent();
+        EnteredPauseScreen ??= new UnityEvent();
+        EnteredInvincible ??= new UnityEvent();
+        EnteredFireFlower ??= new UnityEvent();
+        EnteredDead ??= new UnityEvent();
+        EnteredWon ??= new UnityEvent();
+    }
 
     void Start()
     {
@@ -32,6 +52,7 @@ public class StateManager : MonoBehaviour
         if (CurrentState == GameState.Play)
             return;
         CurrentState = GameState.Play;
+        EnteredPlay.Invoke();
     }
 
     public static void SetStartState()
@@ -39,6 +60,7 @@ public class StateManager : MonoBehaviour
         if (CurrentState == GameState.StartScreen)
             return;
         CurrentState = GameState.StartScreen;
+        EnteredStartScreen.Invoke();
     }
 
     public static void SetPauseState()
@@ -46,13 +68,15 @@ public class StateManager : MonoBehaviour
         if (CurrentState == GameState.PauseScreen)
             return;
         CurrentState = GameState.PauseScreen;
+        EnteredPauseScreen.Invoke();
     }
 
     public static void SetInvincibleState()
     {
-        if (CurrentState == GameState.Invinsible)
+        if (CurrentState == GameState.Invincible)
             return;
-        CurrentState = GameState.Invinsible;
+        CurrentState = GameState.Invincible;
+        EnteredInvincible.Invoke();
     }
 
     public static void SetFireFlowerState()
@@ -60,6 +84,7 @@ public class StateManager : MonoBehaviour
         if (CurrentState == GameState.FireFlower)
             return;
         CurrentState = GameState.FireFlower;
+        EnteredFireFlower.Invoke();
     }
 
     public static void SetDeadState()
@@ -67,6 +92,7 @@ public class StateManager : MonoBehaviour
         if (CurrentState == GameState.Dead)
             return;
         CurrentState = GameState.Dead;
+        EnteredDead.Invoke();
     }
 
     public static void SetWinState()
@@ -74,5 +100,6 @@ public class StateManager : MonoBehaviour
         if (CurrentState == GameState.Won)
             return;
         CurrentState = GameState.Won;
+        EnteredWon.Invoke();
     }
 }
