37bd637fa109bb046f2bb4f033f36cece2d92f8d
benhoweUTS <112325973+benhoweUTS@users.noreply.github.com>
2026-02-25 19:14:08 +1100

    Koopa logic

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private float movespeed = 2f;
     private Vector2 moveDir;
     private Rigidbody2D rb;
     private Collider2D collider;
     private Animator anim;
+    
+    [SerializeField] private EnemyType enemy;
+    [SerializeField] private int score;
+    [SerializeField] private float movespeed = 2f;
     [SerializeField] private float wallCheckDist = 0.1f;
     [SerializeField] private float deathDelay = 2f;
     [SerializeField] private float knockbackForceX;
     [SerializeField] private float knockbackForceY;
+    
+    [SerializeField] private float shellMovespeed = 6f;
+    [SerializeField] private float shellReformDelay = 5f;
+    [SerializeField] private float shellReformTime = 2f;
     
     private enum EnemyType
     {
         Goomba,
         KoopaTroopa
     }
-
-    [SerializeField] private EnemyType enemy;
-    private int score;
     private bool idle;
-    private bool isShell;
+    private bool isDead;
+    private enum KoopaState
+    {
+        Walking,
+        ShellMoving,
+        ShellIdle
+    }
+
+    private KoopaState koopaState;
+    private Coroutine koopaReformCoroutine;
     
     void Start()
     {
@@ -31,18 +44,9 @@ public class EnemyController : MonoBehaviour
         collider = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         idle = true;
-        isShell = false;
+        isDead = false;
         moveDir = Vector2.left;
-        
-        switch (enemy)
-        {
-            case EnemyType.Goomba:
-                score = 100;
-                break;
-            case EnemyType.KoopaTroopa:
-                score = 200;
-                break;
-        }
+        koopaState = KoopaState.Walking;
     }
 
     private void OnBecameVisible()
@@ -59,12 +63,40 @@ public class EnemyController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isDead) return; 
         if (idle) return;
+        if (enemy == EnemyType.KoopaTroopa)
+        {
+            switch (koopaState)
+            {
+                case KoopaState.Walking:
+                    Move(movespeed);
+                    break;
+                case KoopaState.ShellMoving:
+                    Move(shellMovespeed);
+                    break;
+                case KoopaState.ShellIdle:
+                    rb.linearVelocityX = 0f;
+                    break;
+            }
+        }
+        else
+        {
+            Move(movespeed);
+        }
+    }
+
+    private void Move(float ms)
+    {
         Vector2 origin = collider.bounds.center;
         origin.x += moveDir.x * (collider.bounds.extents.x + 0.01f);
         RaycastHit2D hit = Physics2D.Raycast(origin, moveDir, wallCheckDist);
-        if(hit.collider && !hit.collider.CompareTag("Player") && !hit.collider.CompareTag("Projectile")) moveDir *= -1f;
-        rb.linearVelocity = new Vector2(moveDir.x * movespeed, rb.linearVelocityY);
+        
+        if (hit.collider && !hit.collider.CompareTag("Player") && !hit.collider.CompareTag("Projectile"))
+        {
+            moveDir *= -1f;
+        }
+        rb.linearVelocity = new Vector2(moveDir.x * ms, rb.linearVelocityY);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -83,17 +115,20 @@ public class EnemyController : MonoBehaviour
                 switch (enemy)
                 {
                     case EnemyType.Goomba:
-                        DieStomp();
+                        GoombaStomp();
                         break;
                     case EnemyType.KoopaTroopa:
-                        ShellTransform();
+                        KoopaStomp(other.transform);
                         break;
                 }
 
             } else
             {
-                // left, right or below -> damage player
-                
+                if (koopaState == KoopaState.ShellIdle)
+                {
+                    StartMoveShell(other.transform);
+                }
+                //damage player -> player gets i-frames
             }
         } else if (other.gameObject.CompareTag("Projectile")) //projectile being a fireball or shell
         {
@@ -104,7 +139,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private void DieStomp() //only used for Goomba
+    private void GoombaStomp() //only used for Goomba
     {
         
         rb.linearVelocity = Vector2.zero;
@@ -116,32 +151,72 @@ public class EnemyController : MonoBehaviour
         Despawn();
     }
 
+    private void KoopaStomp(Transform player)
+    {
+        switch (koopaState)
+        {
+            case KoopaState.Walking:
+                ShellTransform();
+                break;
+            case KoopaState.ShellMoving:
+                StopShell();
+                break;
+            case KoopaState.ShellIdle:
+                StartMoveShell(player);
+                break;
+        }
+    }
     private void ShellTransform()
     {
-        rb.linearVelocity = Vector2.zero;
-        if (isShell)
+        rb.linearVelocityX = 0f;
+        koopaState = KoopaState.ShellIdle;
+        anim.SetBool("isMoving", false);
+        anim.SetBool("isShell", true);
+        
+        koopaReformCoroutine = StartCoroutine(ShellReform());
+    }
+
+    private void StopShell()
+    {
+        rb.linearVelocityX = 0f;
+        koopaState = KoopaState.ShellIdle;
+        koopaReformCoroutine = StartCoroutine(ShellReform());
+    }
+
+    private void StartMoveShell(Transform player)
+    {
+        koopaState = KoopaState.ShellMoving;
+        //change tag to projectile -> change back to enemy if stop moving
+        if (koopaReformCoroutine != null) StopCoroutine(koopaReformCoroutine);
+        anim.SetBool("isShell", true);
+        anim.SetBool("isReforming", false);
+        
+        if (player.position.x < transform.position.x)
         {
-            
-            
+            moveDir = Vector2.right;
         }
         else
         {
-            anim.SetBool("isMoving", false);
-            anim.SetBool("isShell", true);
-            isShell = true;
+            moveDir = Vector2.left;
         }
     }
     
-    /*private IEnumerator ShellReform(float reformTime){
-        anim.SetBool("isShell", false);
-        anim.SetBool("isReforming", true);
-
-        yield return new WaitForSeconds(reformTime);
-        if (rb.linearVelocity == Vector2.zero)
+    private IEnumerator ShellReform(){
+        yield return new WaitForSeconds(shellReformDelay);
+        if (koopaState == KoopaState.ShellIdle)
+        {
+            anim.SetBool("isShell", false);
+            anim.SetBool("isReforming", true);
+        }
+        yield return new WaitForSeconds(shellReformTime);
+        if (koopaState == KoopaState.ShellIdle)
         {
             anim.SetBool("isReforming", false);
+            anim.SetBool("isMoving", true);
         }
-    }*/
+        koopaState = KoopaState.Walking;
+        moveDir = Vector2.left;
+    }
 
     private void DieKnockback(Vector2 dir)
     {
