30e906fd60df70417225e2a67fc8efeaa9c2eb27
Arie Maljers <ariemaljers@gmail.com>
2026-02-26 21:58:05 +1100

    Flag Pole Cutscenes
    Movement is entrirely done, need only add audio.

--- Assets/Scripts/FlagpoleInteraction.cs
+++ Assets/Scripts/FlagpoleInteraction.cs
@@ -3,16 +3,39 @@ using UnityEngine;
 
 public class FlagpoleInteraction : MonoBehaviour
 {
-    public GameObject ClimbPoint;
-    public GameObject CastlePoint;
+    [SerializeField]
+    private Transform ClimbPoint;
+
+    [SerializeField]
+    private Transform CastlePoint;
+
+    [SerializeField]
+    private Transform GroundPoint;
 
     [SerializeField]
     Animation poleSlide;
 
     public float flagX;
 
+    private bool flagDropped;
+
+    [SerializeField]
+    private Transform flag;
+
+    [SerializeField]
+    private Transform castleFlag;
+
+    [SerializeField]
+    private Vector3 flagBasePos = new Vector3();
+
+    [SerializeField]
+    private Vector3 cFlagEndPos = new Vector3();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() { }
+    void Start()
+    {
+        StateManager.EnteredWon.AddListener(MoveFlagToBase);
+    }
 
     // Update is called once per frame
     void Update() { }
@@ -40,18 +63,79 @@ public class FlagpoleInteraction : MonoBehaviour
     {
         Debug.Log("Moving Mario");
         float newY = 0;
+        float newX = 0;
 
-        while (Mathf.Abs(t.position.y - ClimbPoint.transform.position.y) > 0.01f)
+        while (Mathf.Abs(t.position.y - ClimbPoint.position.y) > 0.01f)
         {
-            newY = Mathf.MoveTowards(
-                t.position.y,
-                ClimbPoint.transform.position.y,
-                2f * Time.deltaTime
-            );
+            newY = Mathf.MoveTowards(t.position.y, ClimbPoint.position.y, 3.5f * Time.deltaTime);
             t.position = new Vector3(t.position.x, newY, 0);
             yield return null;
         }
-        Debug.Log("Stopping Animation");
+        a.SetBool("isPoleSliding", false);
         a.speed = 0;
+        yield return new WaitUntil(() => flagDropped);
+        a.gameObject.transform.Rotate(new Vector3(0, 180, 0));
+        a.gameObject.transform.position += new Vector3(0.38f, 0, 0);
+        yield return new WaitForSeconds(0.5f);
+        a.ResetControllerState();
+        a.speed = 1;
+        a.SetBool("isJumping", true);
+        a.gameObject.transform.Rotate(new Vector3(0, -180, 0));
+
+        while (
+            Mathf.Abs(t.position.y - GroundPoint.transform.position.y) > 0.01f
+            || Mathf.Abs(t.position.x - GroundPoint.transform.position.x) > 0.01f
+        )
+        {
+            newY = Mathf.MoveTowards(t.position.y, GroundPoint.position.y, 3f * Time.deltaTime);
+            newX = Mathf.MoveTowards(t.position.x, GroundPoint.position.x, 2f * Time.deltaTime);
+            t.position = new Vector3(newX, newY, 0);
+            yield return null;
+        }
+
+        a.SetBool("isJumping", false);
+        a.SetFloat("MoveSpeed", 2);
+        a.SetBool("isMoving", true);
+        while (Mathf.Abs(t.position.x - CastlePoint.transform.position.x) > 0.01f)
+        {
+            newX = Mathf.MoveTowards(t.position.x, CastlePoint.position.x, 2f * Time.deltaTime);
+            t.position = new Vector3(newX, t.position.y, 0);
+            yield return null;
+        }
+        MoveFlagOutOfCastle();
+    }
+
+    private void MoveFlagToBase()
+    {
+        StartCoroutine(MoveFlagToBaseEn());
+    }
+
+    IEnumerator MoveFlagToBaseEn()
+    {
+        Debug.Log("MovingFlag");
+        float newY = 0;
+        while (Mathf.Abs(flag.position.y - flagBasePos.y) > 0.01f)
+        {
+            newY = Mathf.MoveTowards(flag.position.y, flagBasePos.y, 2f * Time.deltaTime);
+            flag.position = new Vector3(flag.position.x, newY, 0);
+            yield return null;
+        }
+        flagDropped = true;
+    }
+
+    private void MoveFlagOutOfCastle()
+    {
+        StartCoroutine(MoveFlagOutOfCastleEn());
+    }
+
+    IEnumerator MoveFlagOutOfCastleEn()
+    {
+        float newY = 0;
+        while (Mathf.Abs(castleFlag.position.y - cFlagEndPos.y) > 0.01f)
+        {
+            newY = Mathf.MoveTowards(castleFlag.position.y, cFlagEndPos.y, 2f * Time.deltaTime);
+            castleFlag.position = new Vector3(castleFlag.position.x, newY, 0);
+            yield return null;
+        }
     }
 }
